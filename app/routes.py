3783cedb7427b640d34e70e332eb2878cc1916f5
Yulia Alekseeva <julia4406@gmail.com>
2022-06-21 21:13:12 +0300

    5commit: add 'Register New User'

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
@@ -9,7 +9,6 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Anywhere'}
     posts = [
     {
         'author': {'username': 'John'},
@@ -36,7 +35,7 @@ def index():
         'body': 'Давайте хлопці, я з вами!'
     }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts )
+    return render_template('index.html', title='Home', posts=posts )
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -52,7 +51,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(url_for('next_page'))
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form )
 
 
@@ -60,3 +59,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form )
