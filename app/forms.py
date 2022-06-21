a98b3b79f899b97814adae28ff780ce87cca14c8
Yulia Alekseeva <julia4406@gmail.com>
2022-06-21 22:32:02 +0300

    6 commit: User's page and avatars, sub-templates, new columns in db-forms, add Edit in User Page

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
+    #форма входа зарегистрированного юзера
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
@@ -11,6 +12,7 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    #форма регистрации нового юзера
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -26,3 +28,9 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    #позволит пользователю редактировать
+    username = StringField('Username', validators = [DataRequired()])
+    about_me = TextAreaField('About Me', validators = [Length(min=0, max=140)])
+    submit = SubmitField('Submit')
